13523eace9279b8397e6afbac2bee2e61d7c26e7
Jakhongir <jakhongirsherjonov@gmail.com>
2023-05-17 17:17:11 +0500

    Interview-Preparation-WAY

---
+++ design-patterns/Creational Design Patterns/builder/practice/BuilderPattern/src/main/java/uz/builderpatternservice/user/MainPolly.java
@@ -0,0 +1,29 @@
+package uz.builderpatternservice.user;
+
+public class MainPolly {
+    public static void main(String[] args) {
+        User user1 = new User.UserBuilder("Jahongir", "Nodirxo'ja")
+                .age(30)
+                .phone("1234567")
+                .address("Fake address 1234")
+                .build();
+
+        System.out.println(user1);
+
+        User user2 = new User.UserBuilder("Sanjar", "Hamdamboy")
+                .age(40)
+                .phone("5655")
+                //no address
+                .build();
+
+        System.out.println(user2);
+
+        User user3 = new User.UserBuilder("Abbos", "Shavkat")
+                //No age
+                //No phone
+                //no address
+                .build();
+
+        System.out.println(user3);
+    }
+}
